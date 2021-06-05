e8f84997b182d4fa398520dfc2555739839c4c4f
Solohaga Lukas <solohagalukas@gmail.com>
2021-06-05 00:55:02 -0300

    Requerimiento 2

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -4,6 +4,7 @@ class Tarea(val horasEstimadas :Int, val responsable :Responsable) {
     val empleados = mutableListOf<Empleado>()
 
     fun nominaEmpleados() = empleados + responsable
+    fun horasNecesarias() = horasEstimadas / empleados.size
 }
 
 class Empleado(val sueldoPorHora :Int) {
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -5,14 +5,19 @@ import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
   describe("Una tarea") {
+    val responsable = Responsable(600)
+    val tarea = Tarea(300,responsable)
+    val empleado1 = Empleado(500)
+    val empleado2 = Empleado(500)
+    tarea.empleados.add(empleado1)
+    tarea.empleados.add(empleado2)
+
     it("nomina de empleados") {
-      val responsable = Responsable(600)
-      val tarea = Tarea(300,responsable)
-      val empleado1 = Empleado(500)
-      val empleado2 = Empleado(500)
-      tarea.empleados.add(empleado1)
-      tarea.empleados.add(empleado2)
       tarea.nominaEmpleados() shouldBe mutableListOf(empleado1,empleado2,responsable)
+    }
+
+    it("Horas necesarias, 300 horasEstimadas / 2 empleados") {
+      tarea.horasNecesarias() shouldBe 150
     }
 
   }

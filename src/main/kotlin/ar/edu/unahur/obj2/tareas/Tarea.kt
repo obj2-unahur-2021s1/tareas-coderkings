175d8206ae99d1db83fc49444f4fc19c81d2fc27
Solohaga Lukas <solohagalukas@gmail.com>
2021-06-05 00:36:23 -0300

    Requerimiento 1, falta tareas de integracion

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,3 +1,17 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea
+class Tarea(val horasEstimadas :Int, val responsable :Responsable) {
+    val empleados = mutableListOf<Empleado>()
+
+    fun nominaEmpleados() = empleados + responsable
+}
+
+class Empleado(val sueldoPorHora :Int) {
+
+}
+
+class Responsable(val sueldo :Int) {
+
+}
+
+
--- src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/tareas/TareaTest.kt
@@ -1,8 +1,21 @@
 package ar.edu.unahur.obj2.tareas
 
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.shouldBe
 
 class TareaTest : DescribeSpec({
   describe("Una tarea") {
+    it("nomina de empleados") {
+      val responsable = Responsable(600)
+      val tarea = Tarea(300,responsable)
+      val empleado1 = Empleado(500)
+      val empleado2 = Empleado(500)
+      tarea.empleados.add(empleado1)
+      tarea.empleados.add(empleado2)
+      tarea.nominaEmpleados() shouldBe mutableListOf(empleado1,empleado2,responsable)
+    }
+
   }
+
+
 })

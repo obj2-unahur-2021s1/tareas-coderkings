fd3ae2fa548e8a40f95782de5b55b67bb113654e
Cesar Marca <cesar.marca.20@hotmail.com>
2021-06-08 01:30:02 -0300

    Requerimiento 4 terminado + test

--- src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/tareas/Tarea.kt
@@ -1,24 +1,37 @@
 package ar.edu.unahur.obj2.tareas
 
-class Tarea(val horasEstimadas :Int, val responsable :Responsable , val costoInfraestructura :Int) {
-    val empleados = mutableListOf<Empleado>()
+abstract class Tarea(val responsable: Empleado) {
+    abstract fun nominaEmpleados(): List<Empleado>
+    abstract fun horasNecesarias(): Int
+    abstract fun costo(): Int
+}
 
-    fun nominaEmpleados() = empleados + responsable
-    fun horasNecesarias() = horasEstimadas / cantidadEmpleados()
-    fun cantidadEmpleados() = empleados.size
-    fun sueldoPorHoraCadaEmpleado() = empleados.sumBy{ it.sueldoPorHora }
-    fun costo() =
-        horasNecesarias() * sueldoPorHoraCadaEmpleado() +
-        horasEstimadas * responsable.sueldo +
+class TareaBasico(val horasEstimadas: Int, responsable: Empleado , val costoInfraestructura: Int, val empleadosContratados: List<Empleado>): Tarea(responsable) {
+
+    override fun nominaEmpleados() = (empleadosContratados + responsable).toList()
+    override fun horasNecesarias() = horasEstimadas / cantidadEmpleados()
+    fun cantidadEmpleados() = empleadosContratados.size
+    fun sueldoPorHoraAEmpleados() = empleadosContratados.sumBy{ it.sueldoPorHora }
+    fun sueldoPorEmpleados() = horasNecesarias() * sueldoPorHoraAEmpleados()
+    fun sueldoResponsable() = horasEstimadas * responsable.sueldoPorHora
+    override fun costo() =
+        sueldoPorEmpleados() +
+        sueldoResponsable() +
         costoInfraestructura
 }
 
+class TareaDeIntegracion(responsable: Empleado,val subTareas: List<TareaBasico>): Tarea(responsable) {
+    override fun nominaEmpleados() = (nomimaDeSubTareas() + responsable).toList()
+    override fun horasNecesarias() = horasEnTerminarlasTareas() + tiempoDePlanificacion()
+    override fun costo() = costoTotalEnSubTareas() + bonusExtraResponsable()
+
+    fun horasEnTerminarlasTareas() = subTareas.sumBy { it.horasNecesarias() }
+    fun nomimaDeSubTareas() = subTareas.flatMap { it.nominaEmpleados() }
+    fun tiempoDePlanificacion() = horasEnTerminarlasTareas() / 8
+    fun costoTotalEnSubTareas() = subTareas.sumBy { it.costo() }
+    fun bonusExtraResponsable() = costoTotalEnSubTareas() * 3 / 100
+
+}
 class Empleado(val sueldoPorHora :Int) {
 
 }
-
-class Responsable(val sueldo :Int) {
-
-}
-
-
